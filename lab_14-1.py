0aeecd4769289ca799bb7b8f28291af8bca610d5
p22mbonilla <p22mbonilla@fairfieldprep.org>
2022-03-17 10:08:43 -0400

    add file

---
+++ lab_14-1.py
@@ -0,0 +1,20 @@
+# Author MB 03/17/2022
+
+import turtle
+
+window = turtle.Screen()
+window.setup(1000, 1000)
+window.screensize(800, 800)
+window.title("lab 1")
+
+murtle = turtle.Turtle()
+
+murtle.forward(20)
+murtle.right(90)
+murtle.forward(125)
+murtle.right(90)
+murtle.forward(20)
+murtle.right(90)
+murtle.forward(125)
+
+window.mainloop()
---
+++ lab_14-2.py
@@ -0,0 +1,18 @@
+# Author MB 03/17/2022
+
+import turtle
+
+window = turtle.Screen()
+window.setup(500, 500)
+window.screensize(400, 400)
+window.title("Lab 2")
+
+lenny = turtle.Turtle()
+
+lenny.forward(125)
+lenny.right(120)
+lenny.forward(125)
+lenny.right(120)
+lenny.forward(125)
+
+window.mainloop()
